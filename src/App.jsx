2d8f7afd7e9741c1af527c6cb4125c85d76fcede
Pottendla123 <pottendlanandini1@gmail.com>
2026-02-08 00:14:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import WorkoutSessions from "./components/WorkoutSessions";
+import Gallery from "./components/Gallery";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
+import BMICalculator from "./components/BMICalculator";
+import Footer from "./components/Footer";
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Hero />
+      <WorkoutSessions />
+      <Gallery />
+      <Pricing />
+      <Contact />
+      <BMICalculator />
+      <Footer />
+      <ToastContainer theme="dark" position="top-center" />
+    </BrowserRouter>
+  );
+};
+
+export default App;
