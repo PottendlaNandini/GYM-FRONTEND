5cac012d31c421dea2063c5e4de3bbb60d1fbf44
Pottendla123 <pottendlanandini1@gmail.com>
2026-02-08 10:50:58 +0530

    Update backend URL for deployed backend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://gym-backend-1-c5s3.onrender.com/",
         {
           name,
           email,
