f1b804d495654705c5188006644ab43bfb8ce7bf
Pottendla123 <pottendlanandini1@gmail.com>
2026-02-10 22:18:20 +0530

    jj

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "https://gym-backend-production-c0c1.up.railway.app/send/mail",
+        "https://gym-backend-5.onrender.com/send/mail",
         {
           name,
           email,
